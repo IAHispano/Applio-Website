e4e6262cab4e338a739b9f5ffaa13212da2cec7f
bygimenezz <davidalbal123@gmail.com>
2023-09-24 19:01:15 +0200

    fix build

--- app/models/page.tsx
+++ app/models/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Database } from "../types/database";
 import { cookies } from "next/headers";
 
-export default async function HomePage() {
+ async function HomePage() {
   const supabase = createServerComponentClient<Database>({ cookies });
   const {
     data: { session },
@@ -18,33 +18,34 @@ export default async function HomePage() {
     <section >
     <div className="container flex flex-col justify-center items-center pb-8 pt-6 md:py-10 mx-auto text-center max-w-7xl">
     <h1 className="text-9xl font-bold leading-tight tracking-tighter md:text-9xl mt-4 ">Applio <span className="bg-gradient-radial text-transparent bg-clip-text">Bot</span></h1>
-    <p className="mt-4 text-muted-foreground  text-xs md:text-xl mt-14">
+    <p className="mt-4 text-muted-foreground  text-xs md:text-xl">
     These models represent only a sample of our +8000 available models.
     </p>   
     </div>
     <section className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-5 gap-5 py-8 md:py-10 mx-14">
-      {posts?.map((post: any) => {
-        const {
-          name,
-          image_url: imageUrl,
-          created_at: created_at,
-          link
-        } = post;
+        {posts?.map((post: any) => {
+      const {
+        name,
+        image_url: imageUrl,
+        created_at: created_at,
+        link
+      } = post;
 
-        const modelSlug = link;
+      const modelSlug = link;
 
-        return (
-          <Link href={`${link}`} id={link}>
-            <div className="w-full">
-              <ModelCard
-                name={name}
-                imageUrl={imageUrl}
-                created_at={created_at}
-              />
-            </div>
-          </Link>
-        );
-      })}
+      return (
+        <Link key={modelSlug} href={`${link}`} id={link}>
+          <div className="w-full">
+            <ModelCard
+              name={name}
+              imageUrl={imageUrl}
+              created_at={created_at}
+            />
+          </div>
+        </Link>
+      );
+    })}
+
     </section>
     </section>
   );
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function ErrorPage() {
   
   <h1 className="text-9xl font-bold leading-tight tracking-tighter md:text-9xl mt-4 "><span className="bg-gradient-radial text-transparent bg-clip-text">404</span></h1>
   <p className="mt-4 text-muted-foreground mb-24 text-xs md:text-xl">
-  We couldn't find the page you want
+  We couldn&apos;t find the page you want
   </p>
 </section>
   )

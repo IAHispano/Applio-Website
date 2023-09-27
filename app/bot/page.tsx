4041aba04040f27d129d5de190726214dc195f57
soynixxo <nico.agustin.cba@gmail.com>
2023-09-27 16:46:46 -0300

    modified:   app/bot/page.tsx 	new file:   components/ui/dialog.tsx

--- app/bot/page.tsx
+++ app/bot/page.tsx
@@ -9,11 +9,22 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion"
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter
+} from "@/components/ui/dialog"
+
+import { Bot, Search, Apple } from "lucide-react"
 
 export default function ErrorPage() {
   return (
     <section className="container flex flex-col justify-center items-center pb-8 pt-6 md:py-10 mx-auto text-center max-w-7xl">
-      <h1 className="text-9xl font-bold leading-tight tracking-tighter md:text-9xl mt-4 ">
+      <h1 className="text-9xl font-bold leading-tight tracking-tighter md:text-9xl mt-4 bg-gradient-radial text-transparent bg-clip-text">
         Applio
       </h1>
       <p className="mt-4 text-muted-foreground mb-24 text-xs md:text-xl ">
@@ -49,7 +60,72 @@ export default function ErrorPage() {
           </AccordionContent>
         </AccordionItem>
       </Accordion>
-      <Button className="mt-24">
+      <div className="mt-8">
+
+        {/* Applio Canary Bot  */}
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="font-bold hover:bg-orange-300 bg-orange-400 text-black" size="lg">Invite Applio Canary<div className="ml-1"><Apple></Apple></div></Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="font-bold text-2xl">Search models easily 🍏</DialogTitle>
+              <DialogDescription>
+                By inviting our bot to your server, you will able to search +8000 models in your Discord server with this command
+                <div className="bg-gray-900 hover:bg-gray-800 text-orange-200 flex justify-center rounded p-2 mt-4 mb-2 font-bold text-xl css-0">/search [Model Name]</div>
+              </DialogDescription>
+              <DialogFooter>
+                <Button asChild className="bg-orange-400 hover:bg-orange-300 border-0 text-1xl" size="sm" type="submit">
+                  <Link href="https://discord.com/oauth2/authorize?client_id=1151157990477533326&permissions=8&scope=bot" target="blank">Invite</Link>
+                </Button>
+              </DialogFooter>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
+        {/* Applio Beta BOT */}
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="font-bold hover:bg-pink-500 bg-pink-600 text-black ml-5" size="lg">Invite Applio Beta<div className="ml-1"><Apple></Apple></div></Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="font-bold text-2xl">Search models easily 🍏</DialogTitle>
+              <DialogDescription>
+                By inviting our bot to your server, you will able to search +8000 models in your Discord server with this command
+                <div className="bg-gray-900 hover:bg-gray-800 text-pink-400 flex justify-center rounded p-2 mt-4 mb-2 font-bold text-xl css-0">/search [Model Name]</div>
+              </DialogDescription>
+              <DialogFooter>
+                <Button asChild className="bg-pink-600 hover:bg-pink-500 border-0 text-1xl" size="sm" type="submit">
+                  <Link href="https://discord.com/oauth2/authorize?client_id=1150834440973594784&permissions=8&scope=bot" target="blank">Invite</Link>
+                </Button>
+              </DialogFooter>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
+        {/* Applio BOT*/}
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="font-bold hover:bg-green-400 bg-green-500 text-black ml-5" size="lg">Invite Applio<div className="ml-1"><Apple></Apple></div></Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="font-bold text-2xl">Search models easily 🍏</DialogTitle>
+              <DialogDescription>
+                By inviting our bot to your server, you will able to search +8000 models in your Discord server with this command
+                <div className="bg-gray-900 hover:bg-gray-800 text-green-300 flex justify-center rounded p-2 mt-4 mb-2 font-bold text-xl css-0">/search [Model Name]</div>
+              </DialogDescription>
+              <DialogFooter>
+                <Button asChild className="bg-green-400 hover:bg-green-300 border-0 text-1xl" size="sm" type="submit">
+                  <Link href="https://discord.com/oauth2/authorize?client_id=1144714449563955302&permissions=8&scope=bot" target="blank">Invite</Link>
+                </Button>
+              </DialogFooter>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+      <Button variant="default" className="mt-10 ml-[3rem] font-bold">
         <Link href="/models">Do you want to see all our models?</Link>
       </Button>
     </section>

d3257e4a2a1070626c7130d2f0eb09491192ce2b
bygimenezz <davidalbal123@gmail.com>
2023-09-27 23:00:02 +0200

    fix bot page

--- app/bot/page.tsx
+++ app/bot/page.tsx
@@ -24,7 +24,7 @@ import { Bot, Search, Apple } from "lucide-react"
 export default function BotPage() {
   return (
     <section className="container flex flex-col justify-center items-center pb-8 pt-6 md:py-10 mx-auto text-center max-w-7xl">
-    <h1 className="text-9xl font-bold leading-tight tracking-tighter md:text-9xl mt-4 bg-gradient-radial text-transparent bg-clip-text">
+    <h1 className="text-9xl font-bold leading-tight tracking-tighter md:text-8xl mt-4 ">
       Applio
     </h1>
     <p className="mt-4 text-muted-foreground mb-24 text-xs md:text-xl ">
@@ -44,22 +44,16 @@ export default function BotPage() {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>How do I invite the bot to my server?</AccordionTrigger>
+        <AccordionTrigger>Why are there so many bots with the same name?</AccordionTrigger>
         <AccordionContent>
-          Simply click{" "}
-          <span className="text-green-500">
-            <Link href="https://discord.com/api/oauth2/authorize?client_id=1144714449563955302&permissions=277026719809&scope=bot">
-              here
-            </Link>
-          </span>{" "}
-          and follow the steps discord tells you to follow.
+        Discord is still verifying our Bot so Applio is currently limited to 250 servers. To allow more people to add Applio Bot to their server we created Applio Beta and Applio Canary.
         </AccordionContent>
       </AccordionItem>
     </Accordion>
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="default" className="mt-10 ml-[3rem] font-bold">
-          Do you want to see all our models?
+          Invite Applio
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[700px]"> 
@@ -71,13 +65,13 @@ export default function BotPage() {
           </DialogDescription>
           <DialogFooter>
             <Button asChild className="bg-green-400 hover:bg-green-300 border-0 text-1xl" size="sm" type="submit">
-              <Link href="https://discord.com/oauth2/authorize?client_id=1144714449563955302&permissions=8&scope=bot" target="blank">Invite Applio</Link>
+              <Link href="https://discord.com/oauth2/authorize?client_id=1144714449563955302&permissions=2147871809&scope=bot" target="blank">Applio</Link>
             </Button>
             <Button asChild className="bg-green-400 hover:bg-green-300 border-0 text-1xl" size="sm" type="submit">
-              <Link href="https://discord.com/oauth2/authorize?client_id=1144714449563955302&permissions=8&scope=bot" target="blank">Invite Applio Beta</Link>
+              <Link href="https://discord.com/api/oauth2/authorize?client_id=1150834440973594784&permissions=277028849729&scope=bot" target="blank">Applio Beta</Link>
             </Button>
             <Button asChild className="bg-green-400 hover:bg-green-300 border-0 text-1xl" size="sm" type="submit">
-              <Link href="https://discord.com/oauth2/authorize?client_id=1144714449563955302&permissions=8&scope=bot" target="blank">Invite Applio Canary</Link>
+              <Link href="https://discord.com/api/oauth2/authorize?client_id=1151157990477533326&permissions=277026719809&scope=bot" target="blank">Applio Canary</Link>
             </Button>
           </DialogFooter>
         </DialogHeader>

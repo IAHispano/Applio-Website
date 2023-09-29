1a82249d25955c0b2c6eaacbd439450726f4d06a
bygimenezz <davidalbal123@gmail.com>
2023-09-30 01:33:54 +0200

    best seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,21 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="description" content="Applio is a user-friendly fork of Mangio-RVC-Fork/RVC, designed to provide an intuitive interface, especially for newcomers." />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta property="og:image" content="<generated>" />
+          <meta property="og:image:type" content="<generated>" />
+          <meta property="og:image:width" content="<generated>" />
+          <meta property="og:image:height" content="<generated>" />
+          <meta name="twitter:image" content="<generated>" />
+          <meta name="twitter:image:type" content="<generated>" />
+          <meta name="twitter:image:width" content="<generated>" />
+          <meta name="twitter:image:height" content="<generated>" />
+        </head>
         <body
           className={cn(
             "min-h-screen bg-background font-sans antialiased",
---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://applio.org',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://applio.org',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://applio.org',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+  ]
+}

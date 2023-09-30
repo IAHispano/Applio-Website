ebe8e997cbcd00760c041638d1684a54fee01e39
bygimenezz <davidalbal123@gmail.com>
2023-09-30 21:32:16 +0200

    fix redirectTo

--- components/auth-button-client.tsx
+++ components/auth-button-client.tsx
@@ -12,7 +12,7 @@ export function AuthButton ({ session }: { session: Session | null }) {
     await supabase.auth.signInWithOAuth({
       provider: 'discord',
       options: {
-        redirectTo: 'http://localhost:3000/auth/callback'
+        redirectTo: 'http://applio.org/auth/callback'
       }
     })
   }

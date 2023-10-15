2926ef3e3bf0d708cfb8784bfef13a9fc1f7360f
bygimenezz <davidalbal123@gmail.com>
2023-10-15 14:12:20 +0200

    change bot page

--- app/bot/page.tsx
+++ app/bot/page.tsx
@@ -1,27 +1,19 @@
 "use client"
 
-import Link from "next/link"
-
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
-import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  DialogFooter
-} from "@/components/ui/dialog"
+import { Button } from "@nextui-org/react";
 
 import { Bot, Search, Apple } from "lucide-react"
 
 export default function BotPage() {
+  const handleonclick = () => {
+    window.open('https://discord.com/application-directory/1144714449563955302', '_blank');
+  };
   return (
     <section className="container flex flex-col justify-center items-center pb-8 pt-6 md:py-10 mx-auto text-center max-w-7xl my-10">
     <h1 className="text-9xl font-bold leading-tight tracking-tighter md:text-8xl mt-4 ">
@@ -44,39 +36,17 @@ export default function BotPage() {
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger>Why are there so many bots with the same name?</AccordionTrigger>
+        <AccordionTrigger>In which languages is it available?</AccordionTrigger>
         <AccordionContent>
-        Discord is still verifying our Bot so Applio is currently limited to 250 servers. To allow more people to add Applio Bot to their server we created Applio Beta and Applio Canary.
+        Currently in English and Spanish. Don&apos;t worry, we are still working to expand it to more languages.
         </AccordionContent>
       </AccordionItem>
     </Accordion>
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="default" className="mt-10 ml-[3rem] font-bold">
+    <div className="m-8">
+    <Button size="lg" variant="ghost" onClick={handleonclick}>
           Invite Applio
         </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[700px] max-w-[300px]"> 
-        <DialogHeader>
-          <DialogTitle className="font-bold text-2xl">Search models easily 🍏</DialogTitle>
-          <DialogDescription>
-            By inviting our bot to your server, you will able to search +8000 models in your Discord server with this command
-            <div className="bg-gray-900 hover:bg-gray-800 text-green-300 flex justify-center rounded p-2 mt-4 mb-2 font-bold text-xl css-0">/search [Model Name]</div>
-          </DialogDescription>
-          <DialogFooter>
-            <Button asChild className="bg-green-400 hover:bg-green-300 border-0 text-1xl mb-4" size="sm" type="submit">
-              <Link href="https://discord.com/oauth2/authorize?client_id=1144714449563955302&permissions=2147871809&scope=bot" target="blank">Applio</Link>
-            </Button>
-            <Button asChild className="bg-green-400 hover:bg-green-300 border-0 text-1xl mb-4" size="sm" type="submit">
-              <Link href="https://discord.com/api/oauth2/authorize?client_id=1150834440973594784&permissions=277028849729&scope=bot" target="blank">Applio Beta</Link>
-            </Button>
-            <Button asChild className="bg-green-400 hover:bg-green-300 border-0 text-1xl mb-4" size="sm" type="submit">
-              <Link href="https://discord.com/api/oauth2/authorize?client_id=1151157990477533326&permissions=277026719809&scope=bot" target="blank">Applio Canary</Link>
-            </Button>
-          </DialogFooter>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+    </div>
   </section>
 )
 }

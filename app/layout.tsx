efb3df07ea1aea68b8f5c86712e310600134ab67
bygimenezz <davidalbal123@gmail.com>
2023-10-29 20:02:00 +0100

    twitter og

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export const metadata: Metadata = {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`,
   },
-  description: siteConfig.description,
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
     { media: "(prefers-color-scheme: dark)", color: "black" },

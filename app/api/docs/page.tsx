23820c17e0ca00ba548b382a64c4be856004efba
bygimenezz <davidalbal123@gmail.com>
2023-11-07 01:27:48 +0100

    add api page

---
+++ app/api/docs/page.tsx
@@ -0,0 +1,144 @@
+"use client"
+import { Code, Divider } from "@nextui-org/react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
+
+
+export default function docsApi() {
+  return (
+    <main className="mx-auto max-w-7xl z-10 justify-center items-center mb-12 flex w-3/6">
+        <div className="flex justify-center items-center mx-auto">
+            <div className="col-span-12 lg:col-span-10 xl:col-span-8 lg:px-16 mt-8">
+                <div className="w-full prose prose-neutral mb-10">
+                    <h1 className="text-center flex text-6xl max-md:text-2xl font-semibold text-white mb-2">Applio API</h1>
+                    <p className="mb-3">Welcome to the Applio API documentation! Our API is simple, fast and free, however, before you start using it, it is important that you consult our documentation.</p>
+                    <Divider className="h-1 rounded-xl" />
+                </div>
+                <div className="my-4">
+                <h2 className="text-center flex text-4xl max-md:text-xl font-semibold text-white">API Reference</h2>
+                <Divider className="my-2"/>
+                </div>
+                <article>
+                <h2 className="text-center flex text-4xl max-md:text-xl font-semibold text-white">Get All Models</h2>
+                <p className="my-4">Retrieve information about multiple models using the following API endpoint:</p>
+                <div className="w-full prose prose-neutral my-8">
+                <Code size="lg" className="w-full max-md:text-[9px] break-words"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">/key=(secret)/models/perpage=(number)?type=(kits.ai / rvc)</span></Code>
+                </div>
+                <Table removeWrapper aria-label="Example static collection table">
+                <TableHeader>
+                <TableColumn>PARAMETER</TableColumn>
+                <TableColumn>TYPE</TableColumn>
+                <TableColumn>DESCRIPTION</TableColumn>
+                </TableHeader>
+                <TableBody>
+                <TableRow key="1">
+                    <TableCell>perpage</TableCell>
+                    <TableCell>Number</TableCell>
+                    <TableCell>Required. Number of models to view.</TableCell>
+                </TableRow>
+                <TableRow key="2">
+                    <TableCell>type</TableCell>
+                    <TableCell>String</TableCell>
+                    <TableCell>Optional. Type of model to fetch.</TableCell>
+                </TableRow>
+                </TableBody>
+            </Table>
+                </article>
+                <article>
+                <h2 className="text-center flex text-4xl max-md:text-xl font-semibold text-white mt-10">Search Models</h2>
+                <p className="my-4">Search for specific models by name using the following API endpoint:</p>
+                <div className="w-full prose prose-neutral my-8">
+                <Code size="lg" className="w-full max-md:text-[9px] break-words"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">/key=(secret)/models/search?name=(model_name)&type=(kits.ai / rvc)</span></Code>
+                </div>
+                <Table removeWrapper aria-label="Example static collection table">
+                <TableHeader>
+                <TableColumn>PARAMETER</TableColumn>
+                <TableColumn>TYPE</TableColumn>
+                <TableColumn>DESCRIPTION</TableColumn>
+                </TableHeader>
+                <TableBody>
+                <TableRow key="1">
+                    <TableCell>search</TableCell>
+                    <TableCell>String</TableCell>
+                    <TableCell>Required. Name of model to fetch</TableCell>
+                </TableRow>
+                <TableRow key="2">
+                    <TableCell>type</TableCell>
+                    <TableCell>String</TableCell>
+                    <TableCell>Optional. Type of model to fetch.</TableCell>
+                </TableRow>
+                </TableBody>
+            </Table>
+                </article>
+                <article>
+                <h2 className="text-center flex text-4xl max-md:text-xl font-semibold text-white mt-10">Search User Models</h2>
+                <p className="my-4">To find specific models based on their associated usernames:</p>
+                <div className="w-full prose prose-neutral my-8">
+                <Code size="lg" className="w-full max-md:text-[9px] break-words"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">/key=(secret)/models/user=(username)?type=(kits.ai / rvc)</span></Code>
+                </div>
+                <Table removeWrapper aria-label="Example static collection table">
+                <TableHeader>
+                <TableColumn>PARAMETER</TableColumn>
+                <TableColumn>TYPE</TableColumn>
+                <TableColumn>DESCRIPTION</TableColumn>
+                </TableHeader>
+                <TableBody>
+                <TableRow key="1">
+                    <TableCell>user</TableCell>
+                    <TableCell>String</TableCell>
+                    <TableCell>Required. User owner of the models you want to see.</TableCell>
+                </TableRow>
+                <TableRow key="2">
+                    <TableCell>type</TableCell>
+                    <TableCell>String</TableCell>
+                    <TableCell>Optional. Type of model to fetch.</TableCell>
+                </TableRow>
+                </TableBody>
+            </Table>
+                </article>
+                <article>
+                <h2 className="text-center flex text-4xl max-md:text-xl font-semibold text-white mt-10">Check API Status</h2>
+                <p className="my-4">Check the latency of the API.</p>
+                <div className="w-full prose prose-neutral my-8">
+                <Code size="lg" className="w-full max-md:text-[9px] break-words"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">/ping</span></Code>
+                </div>
+                <p className="my-8 font-bold">Make sure to replace (secret) in the API endpoints with your actual API KEY to authenticate your requests.</p>
+                </article>
+                <div className="my-4">
+                <h2 className="text-center flex text-4xl max-md:text-xl font-semibold text-white">Performance Testing</h2>
+                <Divider className="my-2"/>
+                <p className="my-4">In the table below, you will find the results of our performance tests for the Applio API. We run these tests with every new release, and you can access all the logs here:</p>
+                <Table isStriped aria-label="Example static collection table">
+                <TableHeader>
+                <TableColumn>REQUEST COUNT</TableColumn>
+                <TableColumn>RESPONSE TIME</TableColumn>
+                <TableColumn>VERSION</TableColumn>
+                </TableHeader>
+                <TableBody>
+                <TableRow key="1">
+                    <TableCell>100</TableCell>
+                    <TableCell>1.0s</TableCell>
+                    <TableCell>Beta</TableCell>
+                </TableRow>
+                <TableRow key="2">
+                    <TableCell>250</TableCell>
+                    <TableCell>1.1s</TableCell>
+                    <TableCell>Beta</TableCell>
+                </TableRow>
+                <TableRow key="3">
+                    <TableCell>500</TableCell>
+                    <TableCell>1.4s</TableCell>
+                    <TableCell>Beta</TableCell>
+                </TableRow>
+                <TableRow key="4">
+                    <TableCell>1000</TableCell>
+                    <TableCell>1.6s</TableCell>
+                    <TableCell>Beta</TableCell>
+                </TableRow>
+                </TableBody>
+            </Table>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+}
---
+++ app/api/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { Button, Link } from "@nextui-org/react";
+
+export default function api() {
+  return (
+    <main className="min-h-screen flex flex-col justify-start items-center py-8 md:w-5/6 mx-auto px-5">
+    <div className="grid grid-cols-1 md:grid-cols-8 w-full grid-rows-1 gap-5 gtransition">
+        <article className="md:col-span-8 overflow-hidden md:h-72 relative md:hover:h-96 md:hover:-mx-5 md:hover:w-[calc(100%+40px)] md:hover:rounded-[2rem] gtransition w-full h-full p-5 gap-4 flex flex-col rounded-3xl border-2 border-white/20 bg-black justify-start items-start relative">
+            <div id="h-full flex flex-col justify-center items-center bg-black w-full p-10 absolute overflow-hidden blur-lg scale-110 -ml-5 -mt-5 gtransition" >
+                <img className="absolute top-0 left-0 pointer-events-none h-full w-full object-cover blur-3xl opacity-70 hover:opacity-75" src="https://camo.githubusercontent.com/bffdf5c096954637ef527b596a05e13d3cc598423fa029a2b5d3d3ce18fb91e0/68747470733a2f2f646f63732e6170706c696f2e6f72672f6173736574732f6170706c696f2e706e67"></img>
+            </div>
+            <div className="flex flex-col justify-center items-center text-center gap-4 w-full h-full z-[2] p-5">
+                <h2 className="text-5xl md:text-6xl md:hover:text-8xl font-bold tracking-tighter gtransition">Introducing Applio API</h2>
+                <p>Now create, experiment and developer: free, easy and fast.</p>
+                <div className="flex flex-col-2 gap-4">
+                <Button color="primary" className="hover:scale-110 -bottom-4" as={Link} href="https://docs.google.com/forms/d/e/1FAIpQLSd0YLF8D4n2KNzxDXDfQXhwrQ0KaIGg-Urezlbcix3lKJVHrg/viewform?usp=send_form" target="_blank">Request Access</Button>
+                <Button color="primary" variant="faded" className="hover:scale-110 -bottom-4" as={Link} href="/api/docs">Read Docs</Button>
+                </div>
+            </div>
+        </article>
+        <article className="md:col-span-3 w-full h-full p-5 gap-4 flex flex-col rounded-3xl undefined bg-black justify-start items-start relative border-2 border-white/5">
+            <section className="w-full flex flex-col gap-2 rounded-lg min-h-[13rem] max-h-[16rem] flex-grow overflow-y-auto max-md:min-h-[8rem]">
+                <p className="text-center w-full h-full justify-center items-center flex text-5xl max-md:text-2xl font-semibold text-white px-5 break-words">+500.000 Models</p>
+            </section>
+        </article>
+        <article className="md:col-span-5 w-full h-full p-5 gap-4 flex flex-col rounded-3xl undefined bg-black justify-start items-start relative border-2 border-white/5">
+            <section className="w-full flex flex-col gap-2 rounded-lg min-h-[13rem] max-h-[16rem] flex-grow overflow-y-auto max-md:min-h-[8rem]">
+                <p className="text-center w-full h-full justify-center items-center flex text-5xl max-md:text-2xl font-semibold text-white px-5 " style={{ wordWrap: 'break-word' }}><span className="underline underline-offset-8 decoration-green-500 italic decoration-4 mx-4 md:hover:text-7xl gtransition">-1s</span> Response Time</p>
+            </section>
+        </article>
+        <article className="md:col-span-5 w-full h-full p-5 gap-4 flex flex-col rounded-3xl undefined bg-black justify-start items-start relative border-2 border-white/5">
+            <section className="w-full flex flex-col gap-2 rounded-lg min-h-[13rem] max-h-[16rem] flex-grow overflow-y-auto max-md:min-h-[8rem]">
+                <p className="text-center w-full h-full justify-center items-center flex text-5xl max-md:text-2xl font-semibold text-white px-5 " style={{ wordWrap: 'break-word' }}>Completely <span className="underline underline-offset-8 decoration-green-500 italic decoration-4 mx-4 md:hover:text-7xl gtransition">free</span></p>
+            </section>
+        </article>
+        <article className="md:col-span-3 w-full h-full p-5 gap-4 flex flex-col rounded-3xl undefined bg-black justify-start items-start relative border-2 border-white/5">
+            <section className="w-full flex flex-col gap-2 rounded-lg min-h-[13rem] max-h-[16rem] flex-grow overflow-y-auto max-md:min-h-[8rem]">
+                <p className="text-center w-full h-full justify-center items-center flex text-5xl max-md:text-2xl font-semibold text-white px-5 " style={{ wordWrap: 'break-word' }}>Highly functional</p>
+            </section>
+        </article>
+    </div>
+    </main>
+  )
+}

04d7301cd648642f41ac76b32be2bd0c9fd362dd
bygimenezz <davidalbal123@gmail.com>
2023-11-11 16:21:17 +0100

    fix

--- app/api/docs/page.tsx
+++ app/api/docs/page.tsx
@@ -5,25 +5,25 @@ import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@
 
 export default function docsApi() {
   return (
-    <main className="mx-auto max-md:mx-4 md:max-w-7xl z-10 justify-center items-center mb-12 flex w-3/6 ">
+    <main className="mx-auto md:max-w-7xl z-10 justify-center items-center mb-12 flex flex-col items-center max-md:mx-4">
         <div className="flex justify-center items-center mx-auto">
             <div className="mt-8">
                 <div className="w-full prose prose-neutral mb-10">
-                    <h1 className="text-center flex text-6xl max-md:text-2xl font-semibold dark:text-white mb-4">Applio API</h1>
-                    <p className="mb-3">Welcome to the Applio API documentation! Our API is simple, fast and free, however, before you start using it, it is important that you consult our documentation.</p>
+                    <h1 className="text-center flex text-6xl max-md:text-2xl font-semibold dark:text-white mb-4 ">Applio API</h1>
+                    <p className="mb-3 break-words max-md:max-w-sm">Welcome to the Applio API documentation! Our API is simple, fast and free, however, before you start using it, it is important that you consult our documentation.</p>
                     <Divider className="h-1 rounded-xl" />
                 </div>
                 <div className="my-4">
                 <h2 className="text-center flex text-4xl max-md:text-xl font-semibold dark:text-white">API Reference</h2>
                 <Divider className="my-2"/>
                 </div>
-                <article>
+                <article  className="max-md:max-w-sm">
                 <h2 className="text-center flex text-4xl max-md:text-xl font-semibold dark:text-white">Get All Models</h2>
-                <p className="my-4">Retrieve information about multiple models using the following API endpoint:</p>
-                <div className="w-full prose prose-neutral my-8">
-                <Code size="lg" className="w-full max-md:text-[9px] break-words select-all "><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">https://api.applio.org/key=(secret)/models/perpage=(number)/page=(number)?type=(kits.ai / rvc)</span></Code>
+                <p className="my-4 max-md:max-w-sm">Retrieve information about multiple models using the following API endpoint:</p>
+                <div className="w-full prose prose-neutral my-8 max-md:max-w-sm hidden md:block">
+                <Code size="lg" className="w-full max-md:text-[9px] break-words select-all max-md:max-w-sm"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">https://api.applio.org/key=(secret)/models/perpage=(number)/page=(number)?type=(string)</span></Code>
                 </div>
-                <Table removeWrapper aria-label="Example static collection table">
+                <Table removeWrapper className="max-md:max-w-sm">
                 <TableHeader>
                 <TableColumn>PARAMETER</TableColumn>
                 <TableColumn>TYPE</TableColumn>
@@ -48,13 +48,13 @@ export default function docsApi() {
                 </TableBody>
             </Table>
                 </article>
-                <article>
+                <article  className="max-md:max-w-sm">
                 <h2 className="text-center flex text-4xl max-md:text-xl font-semibold dark:text-white mt-10">Search Models</h2>
                 <p className="my-4">Search for specific models by name using the following API endpoint:</p>
-                <div className="w-full prose prose-neutral my-8">
+                <div className="w-full prose prose-neutral my-8 hidden md:block">
                 <Code size="lg" className="w-full max-md:text-[9px] break-words select-all"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">https://api.applio.org/key=(secret)/models/search?name=(model_name)&type=(kits.ai / rvc)</span></Code>
                 </div>
-                <Table removeWrapper aria-label="Example static collection table">
+                <Table removeWrapper className="max-md:max-w-sm">
                 <TableHeader>
                 <TableColumn>PARAMETER</TableColumn>
                 <TableColumn>TYPE</TableColumn>
@@ -74,10 +74,10 @@ export default function docsApi() {
                 </TableBody>
             </Table>
                 </article>
-                <article>
+                <article className="max-md:max-w-sm">
                 <h2 className="text-center flex text-4xl max-md:text-xl font-semibold dark:text-white mt-10">Search User Models</h2>
                 <p className="my-4">To find specific models based on their associated usernames:</p>
-                <div className="w-full prose prose-neutral my-8">
+                <div className="w-full prose prose-neutral my-8 hidden md:block">
                 <Code size="lg" className="w-full max-md:text-[9px] break-words select-all"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">https://api.applio.org/key=(secret)/models/user=(username)?type=(kits.ai / rvc)</span></Code>
                 </div>
                 <Table removeWrapper aria-label="Example static collection table">
@@ -103,12 +103,12 @@ export default function docsApi() {
                 <article>
                 <h2 className="text-center flex text-4xl max-md:text-xl font-semibold dark:text-white mt-10">Check API Status</h2>
                 <p className="my-4">Check the latency of the API.</p>
-                <div className="w-full prose prose-neutral my-8">
+                <div className="w-full prose prose-neutral my-8 hidden md:block">
                 <Code size="lg" className="w-full max-md:text-[9px] break-words select-all"><span className="text-[#ff9492]">GET</span> <span className="text-[#91cbff]">https://api.applio.org/ping</span></Code>
                 </div>
                 <p className="my-8 font-bold">Make sure to replace (secret) in the API endpoints with your actual API KEY to authenticate your requests.</p>
                 </article>
-                <div className="my-4">
+                <div className="my-4 max-md:max-w-sm">
                 <h2 className="text-center flex text-4xl max-md:text-xl font-semibold dark:text-white">Performance Testing</h2>
                 <Divider className="my-2"/>
                 <p className="my-4">In the table below, you will find the results of our performance tests for the Applio API. We run these tests with every new release, and you can access all the logs here:</p>

19b27a75783a23d15746e6de8a085ccc757c752d
bygimenezz <davidalbal123@gmail.com>
2024-02-04 21:27:00 +0100

    robots.ts + sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,20 +5,75 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://applio.org",
       lastModified: new Date(),
-      changeFrequency: "yearly",
+      changeFrequency: "always",
       priority: 1,
     },
     {
-      url: "https://applio.org",
+      url: "https://applio.org/models",
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: "always",
       priority: 0.8,
     },
     {
-      url: "https://applio.org",
+      url: "https://applio.org/guides",
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "always",
       priority: 0.5,
     },
+    {
+      url: "https://applio.org/playground",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.6,
+    },
+    {
+      url: "https://applio.org/blog",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.8,
+    },
+    {
+      url: "https://applio.org/bot",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.5,
+    },
+    {
+      url: "https://applio.org/api",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.8,
+    },
+    {
+      url: "https://applio.org/team",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.2,
+    },
+    {
+      url: "https://applio.org/privacy",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.1,
+    },
+    {
+      url: "https://applio.org/terms-service",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.1,
+    },
+    {
+      url: "https://applio.org/terms-use",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.1,
+    },
+    {
+      url: "https://applio.org/login",
+      lastModified: new Date(),
+      changeFrequency: "always",
+      priority: 0.5,
+    },
+    
   ]
 }

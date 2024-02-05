4af3a8990db4762562c8b897cd7cc0fda5797d9f
Blaise <blaise@iahispano.com>
2024-02-05 21:47:24 +0100

    fix size

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,11 +1,11 @@
 export default function IndexPage() {
   return (
-    <main className="w-full h-screen flex justify-center items-center pt-20 mb-8 max-md:pt-44 max-md:mb-8">
+    <main className="w-full h-screen flex items-center mt-10">
       <iframe
         src="https://iahispano-applio.hf.space"
         width="100%"
         height="100%"
       ></iframe>
     </main>
-  );
+  )
 }

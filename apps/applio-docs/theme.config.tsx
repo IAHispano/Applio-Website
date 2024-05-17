413d60da992cd5850ea3ad3b591c1af4699395e5
Blaise <blaise@applio.org>
2024-05-17 11:59:26 +0200

    update some pages

--- apps/applio-docs/theme.config.tsx
+++ apps/applio-docs/theme.config.tsx
@@ -3,60 +3,79 @@ import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-	logo: <span className="font-medium tracking-tight">Applio Docs</span>,
-	project: {
-		link: "https://github.com/iahispano/applio",
-	},
-	chat: {
-		link: "https://discord.gg/iahispano",
-	},
-	docsRepositoryBase: "https://github.com/iahispano/applio-docs",
-	footer: {
-		text: "Copyright © 2024 AI Hispano. All rights reserved.",
-	},
-	nextThemes: {
-		defaultTheme: "dark",
-	},
-	feedback: {
-		content: null,
-	},
-	editLink: {
-		component: null,
-	},
-	faviconGlyph: "/favicon.ico",
-	logoLink: "/favicon.ico",
-	head: () => {
-		const { asPath, defaultLocale, locale } = useRouter();
-		const { frontMatter } = useConfig();
-		const url =
-			"https://docs.applio.org" +
-			(defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+  logo: <span className="font-medium tracking-tight">Applio Docs</span>,
+  project: {
+    link: "https://github.com/iahispano/applio",
+  },
+  chat: {
+    link: "https://discord.gg/iahispano",
+  },
+  docsRepositoryBase: "https://github.com/iahispano/applio",
+  footer: {
+    text: (
+      <span>
+        Copyright {new Date().getFullYear()} © Applio. All rights reserved.
+      </span>
+    ),
+  },
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+  feedback: {
+    content: null,
+  },
+  editLink: {
+    component: null,
+  },
+  faviconGlyph: "/favicon.ico",
+  logoLink: "/favicon.ico",
+  banner: {
+    key: "new-release",
+    text: (
+      <a href="https://applio.org" target="_blank">
+        🎉 Applio has evolved into the next generation. Watch more →
+      </a>
+    ),
+  },
+  primarySaturation: 100,
+  primaryHue: 151,
 
-		return (
-			<>
-				<meta property="og:url" content={url} />
-				<link rel="icon" href="/favicon.ico" sizes="any" />
-				<meta
-					property="og:description"
-					content={
-						frontMatter.description || "Documentation for the Applio project."
-					}
-				/>
-				<meta
-					property="og:title"
-					content={
-						frontMatter.title ? `${frontMatter.title} | Applio` : "Applio Docs"
-					}
-				/>
-				<meta property="favicon" content="https://i.imgur.com/ZeuGiOR.png" />
-			</>
-		);
-	},
-	useNextSeoProps() {
-		return {
-			titleTemplate: `%s | Applio`,
-		};
-	},
+  i18n: [
+    { locale: "en-US", text: "English" },
+    { locale: "es-ES", text: "Spanish" },
+  ],
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+    const url =
+      "https://docs.applio.org" +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`);
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description || "Documentation for the Applio project."
+          }
+        />
+        <meta
+          property="og:title"
+          content={
+            frontMatter.title ? `${frontMatter.title} | Applio` : "Applio Docs"
+          }
+        />
+        <meta property="favicon" content="https://i.imgur.com/ZeuGiOR.png" />
+      </>
+    );
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: `%s - Applio`,
+    };
+  },
 };
 
 export default config;

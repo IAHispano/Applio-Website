6ba7b614dc4e6a1c497b779de81b7d29b090f8ba
bygimenezz <davidalbal123@gmail.com>
2024-05-20 23:59:48 +0200

    fix build

--- apps/applio-tv/next.config.mjs
+++ apps/applio-tv/next.config.mjs
@@ -3,8 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    transpilePackages: ["@applio/supabase"],
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);

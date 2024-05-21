e34cdb9cbbe2230c102c5518db8f765090ab2466
bygimenez <bygimenezz@gmail.com>
2024-05-21 23:11:14 +0200

    org: fix cf

--- apps/applio-org/src/app/auth/callback/route.ts
+++ apps/applio-org/src/app/auth/callback/route.ts
@@ -2,6 +2,8 @@ import { supabase } from '@/utils/database'
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest, NextResponse } from 'next/server'
 
+export const runtime = process.env.NODE_ENV === "production" ? 'edge' : undefined;
+
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')

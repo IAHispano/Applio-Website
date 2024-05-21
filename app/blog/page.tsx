b19bc549eda6bf9595e5612ddcb2147fd6223c82
bygimenez <bygimenezz@gmail.com>
2024-05-22 01:55:29 +0200

    remove blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,10 @@
-"use client"
-
-import BlogCard from "@/components/blog/blog-card"
-
-export default function BlogPage() {
+export default function ApiHome() {
   return (
-    <main className="flex flex-col justify-center items-center text-center min-h-[600px]">
-      <section className="md:w-full max-w-7xl p-4 flex flex-col gap-5 rounded-medium max-md:mx-2 justify-center items-center place-items-center">
-        <div className="my-8">
-        {process.env.NEXT_PUBLIC_SUPABASE_URL && process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? (
-            <BlogCard />
-          ) : (
-            <p className="text-neutral-300 text-center h-[400px] flex justify-center items-center text-3xl">Development mode activated</p>
-          )}
-        </div>
+    <main className="flex min-h-screen flex-col items-center max-md:mx-4 md:-mt-28">
+      <section className="z-30 mt-72 text-center flex justify-center items-center flex-col">
+      <h1 className="md:text-6xl lg:max-w-5xl max-w-xl text-4xl font-bold leading-8 md:mb-6">We are working on improving our systems</h1>
+      <h2 className="read-font text-white/80 max-w-lg mx-auto">During the next few weeks, some sections of our website may not be available. We are renewing our projects and improving our systems to offer you new services. We are sorry for the inconvenience.</h2>
       </section>
     </main>
-  )
+  );
 }

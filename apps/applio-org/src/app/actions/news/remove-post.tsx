e9fe27f9341d7ea81e8ad61c9ef9584501e6d7a7
bygimenezz <davidalbal123@gmail.com>
2024-05-24 10:00:11 +0200

    org: try fix

--- apps/applio-org/src/app/actions/news/remove-post.tsx
+++ apps/applio-org/src/app/actions/news/remove-post.tsx
@@ -3,7 +3,10 @@
 import { supabase } from "@/utils/database"
 import { redirect } from "next/navigation"
 
-export const runtime = 'edge';
+export const config = {
+    runtime: 'edge',
+  };
+  
 
 export async function removeNews(id: number) {
     const { error, status } = await supabase

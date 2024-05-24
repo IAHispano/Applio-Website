c9421bb32ad45053000a2172faab0a80456b615e
bygimenez <bygimenezz@gmail.com>
2024-05-25 00:11:44 +0200

    add og

--- apps/applio-org/src/app/news/[id]/page.tsx
+++ apps/applio-org/src/app/news/[id]/page.tsx
@@ -1,5 +1,6 @@
 import NewsPostComponent from "@/components/news/NewsPost"
 import AdminDashboard from "@/components/news/admin-post"
+import { supabase } from "@/utils/database";
 
 export const runtime = 'edge';
 
@@ -11,3 +12,27 @@ export default function NewsPost({ params }: { params: { id: string } }) {
             <AdminDashboard id={id as string}/>
         </main>
     )}
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+    const { data, error } = await supabase
+        .from("blog")
+        .select("title, image_url, by, created_at")
+        .eq("title", params.id)
+        .single()
+    
+    if (error) {
+        return {
+        title: 'Applio',
+        }
+    }
+    
+    return {
+        title: data.title,
+        description: `Read ${data.title} created by ${data.by} at ${new Date(data.created_at), 'MMMM do, yyyy'}.`,
+        openGraph: {
+            title: data.title,
+            images: data.image_url,
+            description: `Read ${data.title} created by ${data.by} at ${new Date(data.created_at), 'MMMM do, yyyy'}.`,
+            }  
+    }
+    }

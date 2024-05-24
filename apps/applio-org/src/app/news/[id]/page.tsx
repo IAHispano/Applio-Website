5fb843859147f77410ebc21f3ffdc3804c29fb34
bygimenezz <davidalbal123@gmail.com>
2024-05-24 10:16:13 +0200

    org: fix cf x2232323123

--- apps/applio-org/src/app/news/[id]/page.tsx
+++ apps/applio-org/src/app/news/[id]/page.tsx
@@ -1,6 +1,8 @@
 import NewsPostComponent from "@/components/news/NewsPost"
 import AdminDashboard from "@/components/news/admin-post"
 
+export const runtime = typeof window === 'undefined' ? 'edge' : 'nodejs';
+
 export default function NewsPost({ params }: { params: { id: number } }) {
     const { id } = params
     return (
--- apps/applio-org/src/app/news/edit/[id]/page.tsx
+++ apps/applio-org/src/app/news/edit/[id]/page.tsx
@@ -1,5 +1,7 @@
 import EditPost from "@/components/news/edit-post"
 
+export const runtime = typeof window === 'undefined' ? 'edge' : 'nodejs';
+
 export default function NewsPost({ params }: { params: { id: number } }) {
     const { id } = params
     return (

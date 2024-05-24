cf313a3719ae4a8d14e070467a5207d0ad830aa7
bygimenezz <davidalbal123@gmail.com>
2024-05-24 23:10:28 +0200

    org: try something

--- apps/applio-org/src/app/news/[id]/page.tsx
+++ apps/applio-org/src/app/news/[id]/page.tsx
@@ -1,5 +1,6 @@
 import NewsPostComponent from "@/components/news/NewsPost"
 import AdminDashboard from "@/components/news/admin-post"
+import { supabase } from "@/utils/database";
 
 export const runtime = 'edge';
 
@@ -11,3 +12,24 @@ export default function NewsPost({ params }: { params: { id: number } }) {
             <AdminDashboard id={id as number}/>
         </main>
     )}
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+    const { data, error } = await supabase
+        .from("blog")
+        .select("title, image_url")
+        .eq("id", parseInt(params.id))
+    
+    if (error) {
+        return {
+        title: 'Applio',
+        }
+    }
+    
+    return {
+        title: data[0].title,
+        openGraph: {
+            title: data[0].title,
+            images: data[0].image_url
+          }  
+    }
+    }

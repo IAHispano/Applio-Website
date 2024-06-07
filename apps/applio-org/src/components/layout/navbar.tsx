6d1232237e53aab05a7cbe82d95e62f19ded2bb6
Blaise <blaise@applio.org>
2024-06-07 23:13:21 +0200

    fix(org): svg warnings

--- apps/applio-org/src/components/layout/navbar.tsx
+++ apps/applio-org/src/components/layout/navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
                     <a className="col-span-1 max-md:bg-white/10 max-md:p-4 max-md:rounded-xl max-md:w-full" href="/models">Explore</a>
                     <a className="col-span-1 max-md:bg-white/10 max-md:p-4 max-md:rounded-xl max-md:w-full" href="/news">News</a>
                     <div className="dropdown">
-                        <a className="col-span-1 max-md:bg-white/10 max-md:p-4 max-md:rounded-xl max-md:w-full flex items-center transition">Products<svg className="ml-2 mt-0.5" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.3333 1L5.66667 5.66667L1 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></a>
+                        <a className="col-span-1 max-md:bg-white/10 max-md:p-4 max-md:rounded-xl max-md:w-full flex items-center transition">Products<svg className="ml-2 mt-0.5" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.3333 1L5.66667 5.66667L1 1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg></a>
                         <div className="dropdown-menu hidden absolute bg-[#2f2e31] backdrop-blur-lg border border-white/10 rounded-xl text-white w-[60svh] p-4 shadow-xl mt-0.5">
                             <div className="grid grid-cols-3 w-full divide-x divide-white/20 items-start text-left">
                                 <div className="col-span-1 flex flex-col gap-2 w-full p-2">
@@ -60,7 +60,7 @@ export default function Navbar() {
                         </div>
                     </div>
                     <div className="dropdown">
-                        <a className="col-span-1 max-md:bg-white/10 max-md:p-4 max-md:rounded-xl max-md:w-full flex items-center transition">About Us<svg className="ml-2 mt-0.5" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.3333 1L5.66667 5.66667L1 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></a>
+                        <a className="col-span-1 max-md:bg-white/10 max-md:p-4 max-md:rounded-xl max-md:w-full flex items-center transition">About Us<svg className="ml-2 mt-0.5" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.3333 1L5.66667 5.66667L1 1" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg></a>
                         <div className="dropdown-menu hidden absolute bg-[#2f2e31] border border-white/10 rounded-xl text-white w-[60svh] p-4 shadow-xl mt-0.5">
                             <div className="grid grid-cols-2 w-full divide-x divide-white/20 items-start text-left">
                                 <div className="col-span-1 flex flex-col gap-2 w-full p-2">

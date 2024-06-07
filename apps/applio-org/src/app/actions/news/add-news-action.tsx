8fa16fd8ac0b261f9722cc3b36b4ea24b603c65b
bygimenez <davidalbal123@gmail.com>
2024-06-07 23:05:13 +0200

    fix(org): :bug:

--- apps/applio-org/src/app/actions/news/add-news-action.tsx
+++ apps/applio-org/src/app/actions/news/add-news-action.tsx
@@ -3,12 +3,15 @@
 import { supabase } from "@/utils/database"
 import { redirect } from "next/navigation"
 
+export const config = {
+  runtime: 'edge',
+};
+
 export const addPost = async (formData: FormData, id: number, role: string) => {
   const title = formData.get("title")
   const description = formData.get("description")
   const content = formData.get("content")
   const fixed = formData.get("fixed")
-  const runtime = 'edge';
 
   if (title === null || content === null || fixed === null || description === null) return
 

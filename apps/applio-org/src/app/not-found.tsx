8e595ff8500b2df0382621cd75b0eb1b65bb584b
bygimenez <davidalbal123@gmail.com>
2024-06-08 18:20:32 +0200

    chore(org): :ambulance: add not found page

---
+++ apps/applio-org/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <main className="min-h-[80svh] flex justify-center items-center mx-auto">
+            <h1 className="text-3xl">Page not found (<span className="read-font font-bold">404</span>).</h1>
+        </main>
+    )
+
+}

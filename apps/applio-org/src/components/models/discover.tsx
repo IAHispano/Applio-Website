b17a88a0dc7f05ad10cf3d26933149597d555fc5
bygimenez <davidalbal123@gmail.com>
2024-06-11 03:50:36 +0200

    revert(org): :rewind: revert

--- apps/applio-org/src/components/models/discover.tsx
+++ apps/applio-org/src/components/models/discover.tsx
@@ -196,27 +196,3 @@ export default function DiscoverModels() {
         </>
     )
 }
-
-export async function generateMetadata({ params }: { params: { id: number } }) {
-  const { data, error } = await supabase
-        .from("blog")
-      .select("title, image_url, by, created_at, tag")
-      .eq("id", params.id)
-      .single()
-  
-  if (error) {
-      return {
-      title: 'Applio',
-      }
-  }
-
-  return {
-      title: data.title,
-      description: `Read an article about ${data.tag} created by ${data.by} at ${new Date(data.created_at).toLocaleDateString('en-EN', { year: 'numeric', month: 'long', day: 'numeric' })}.`,
-      openGraph: {
-          title: data.title,
-          images: data.image_url,
-          description: `Read an article about ${data.tag} created by ${data.by} at ${new Date(data.created_at).toLocaleDateString('en-EN', { year: 'numeric', month: 'long', day: 'numeric' })}.`,
-          }  
-  }
-  }

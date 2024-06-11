6b60fc08eb91d00b09609bac342b207490dc680a
bygimenez <davidalbal123@gmail.com>
2024-06-11 23:02:47 +0200

    refactor(org): :chart_with_upwards_trend: add og

--- apps/applio-org/src/app/[id]/page.tsx
+++ apps/applio-org/src/app/[id]/page.tsx
@@ -1,4 +1,34 @@
 import UserCard from "@/components/profile/user-card";
+import { supabase } from "@/utils/database";
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+    const id = params.id;
+
+    if (id) {
+    const { data, error } = await supabase
+        .from("profiles")
+        .select("full_name, avatar_url")
+        .eq("full_name", id)
+        .single();
+  
+    if (error) {
+        return {
+            title: 'Applio',
+        };
+    }
+    if (data) {
+    return {
+        title: `${data.full_name} in Applio.`,
+        description: `See ${data.full_name} profile.`,
+        openGraph: {
+            title:  `${data.full_name} in Applio.`,
+            images: data.avatar_url,
+            description: `See ${data.full_name} profile.`,
+        }  
+    };
+    }
+}
+}
 
 export default function Profile({ params }: { params: { id: string } }) {
     const id = params.id

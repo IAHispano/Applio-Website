8138f4cbeb5591440d3f53dd9e7d77dfa9a45657
bygimenez <davidalbal123@gmail.com>
2024-09-14 16:38:57 +0200

    fix(org): :art: minor change

--- apps/applio-org/src/app/products/app/page.tsx
+++ apps/applio-org/src/app/products/app/page.tsx
@@ -2,65 +2,11 @@ export default function AppProduct() {
     return (
         <main className="w-full min-h-screen p-8">
             <section className="w-full h-[70svh] rounded-xl flex flex-col justify-center items-center p-4 relative mb-24">
-            <svg xmlns="http://www.w3.org/2000/svg" className="max-md:max-w-xs" width="1920" height="1080" viewBox="0 0 1920 1080" fill="none">
-            <g clip-path="url(#clip0_3_24)">
-            <rect width="1920" height="1080" rx="40" fill="#110F0F"/>
-            <g opacity="0.1">
-            <line x1="277" y1="152.5" x2="1643" y2="152.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <line x1="277" y1="231.5" x2="1643" y2="231.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <line x1="277" y1="921.5" x2="1643" y2="921.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <line x1="277" y1="851.5" x2="1643" y2="851.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <line x1="277" y1="792.5" x2="1643" y2="792.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <line x1="430" y1="153.5" x2="430" y2="922.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <line x1="1482" y1="153.5" x2="1482" y2="922.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <path d="M471.5 152.5L571 235.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <path d="M462.5 788.5L276 892M632 788.5L467 926.5M762.5 787.5L661.5 926.5M878 787.5L817.5 925" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <path d="M1443 787L1644.5 904.5M1281.5 787L1445.5 924M1149.5 788L1249.5 926.5M1051 787.5L1097 927.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <path d="M663.5 153L726.5 240M817 152L855.5 238M1095.5 152L1068 238M1248 153L1186 238M1447 153L1347 237.5M1644.5 171.5L1479.5 268.5" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.1">
-            <path d="M273 181L440 274" stroke="" strokeWidth="2"/>
-            </g>
-            <g opacity="0.25" filter="url(#filter0_f_3_24)">
-            <ellipse cx="987.5" cy="1120.5" rx="887.5" ry="680.5" fill="url(#paint0_angular_3_24)"/>
-            </g>
-            </g>
-            <defs>
-            <filter id="filter0_f_3_24" x="-271.2" y="68.8" width="2517.4" height="2103.4" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-            <feGaussianBlur stdDeviation="185.6" result="effect1_foregroundBlur_3_24"/>
-            </filter>
-            <radialGradient id="paint0_angular_3_24" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(987.5 1120.5) rotate(45.3762) scale(769.049 799.044)">
-            <stop offset="0.13" stopColor="#04DE71"/>
-            <stop offset="0.255" stopColor="#00FF75"/>
-            <stop offset="0.59" stopColor="#11FAA6"/>
-            </radialGradient>
-            <clipPath id="clip0_3_24">
-            <rect width="1920" height="1080" rx="40" fill="#110F0F"/>
-            </clipPath>
-            </defs>
-            </svg>
+            <div className="w-full h-full rounded-xl max-w-7xl" 
+            style={{
+                backgroundImage:
+                    "radial-gradient(ellipse 120% 80% at 50% 100%, #00AA68, transparent)",
+            }} />
             <div className="absolute text-center md:text-[8svh] text-5xl font-bold leading-relaxed">
                 <h1>Applio APP</h1>
                 <h2 className="text-sm tracking-widest font-semibold text-neutral-300">Coming soon</h2>

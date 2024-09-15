75cfeaf1c1f24075085834bae915acd9d7b808ae
blaisewf <blaise@applio.org>
2024-09-15 19:59:24 +0200

    fix(org): duplicated env variables

--- apps/applio-org/src/utils/database.ts
+++ apps/applio-org/src/utils/database.ts
@@ -3,5 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_KEY as string);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
  

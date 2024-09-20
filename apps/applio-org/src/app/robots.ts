f64891712603bfeec1ef12f7144a7b3e5483c53c
bygimenez <davidalbal123@gmail.com>
2024-09-20 17:04:01 +0200

    chore(org): :mag: add robots and sitemap

---
+++ apps/applio-org/src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from "next"
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/public/", "/api/", "/_next/"],
+    },
+    sitemap: "https://applio.org/sitemap.xml",
+  }
+}
---
+++ apps/applio-org/src/app/sitemap.ts
@@ -0,0 +1,81 @@
+export const sitemap = [
+    {
+      loc: 'https://applio.org/',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 1.00
+    },
+    {
+      loc: 'https://applio.org/models',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/learn',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/products/rvc',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/products/app',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/products/playground',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/products/api',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/about/team',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/@guides',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/about/branding',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.80
+    },
+    {
+      loc: 'https://applio.org/learn/create',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.64
+    },
+    {
+      loc: 'https://applio.org/products/rvc/changelog',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.64
+    },
+    {
+      loc: 'https://applio.org/settings?p=developer',
+      lastmod: '2024-09-20T14:58:04+00:00',
+      changefreq: 'weekly',
+      priority: 0.64
+    }
+  ];
+  

a29030fa10cd374b03625c2649f52a0111602745
bygimenez <davidalbal123@gmail.com>
2024-09-21 19:12:01 +0200

    chore(org): :memo: change incident-response

--- apps/applio-org/src/app/blog/incident-response/page.tsx
+++ apps/applio-org/src/app/blog/incident-response/page.tsx
@@ -1,9 +1,10 @@
 export default function incidentResponsePage() {
   return (
-    <main className="min-h-screen max-w-5xl flex justify-start items-start mx-auto mt-28 max-md:p-6">
-    <div className="flex flex-col w-full gap-2 max-w-3xl">
+    <main className="min-h-screen max-w-5xl flex justify-center items-start mx-auto mt-28 max-md:p-6">
+    <div className="flex flex-col w-full gap-24">
+      <div>
       <h1 className="md:text-3xl text-2xl text-neutral-200">Incident <span className="read-font">09-21-2024</span></h1>
-      <div className="h-0.5 bg-neutral-600 rounded-xl w-full"/>
+      <div className="h-0.5 bg-neutral-600 rounded-xl w-full my-0.5"/>
       <div className="prose text-neutral-300 text-balance gap-2 mt-4">
         <p>
           We regret to inform you that in the early hours of September 21, <span className="text-white underline">the Discord account of Blaise</span>, the owner of Applio and AI Hispano, was hacked.
@@ -23,6 +24,29 @@ export default function incidentResponsePage() {
           Sincerely,<br />
           The <span className="font-semibold">Applio</span> Team.
         </p>
+      </div>
+      </div>
+      <div>
+      <h1 className="md:text-3xl text-2xl text-neutral-200">About Applio Premium</h1>
+      <div className="h-0.5 bg-neutral-600 rounded-xl w-full my-0.5"/>
+      <div className="prose text-neutral-300 text-balance gap-2 mt-4">
+      <p>
+        We regret to inform you that due to <span className="text-white underline">Stripe's policies</span>, our account was suspended as <span className="text-white underline">they do not allow non-profit organizations to accept donations</span>. <span className="text-white underline">This forced us to</span> close our account and <span className="text-white underline">discontinue our premium service</span>.
+      </p>
+      <p>
+        We are no longer charging our premium users, and <span className="text-white underline">we are actively working on restoring the features</span> that were part of our premium offering.
+      </p>
+      <p>
+        Our main priority is to ensure that all those who have supported us with a donation can once again enjoy the benefits of our service.
+      </p>
+      <p>
+        Thank you for your understanding and continued support.
+      </p>
+      <p className="mt-4">
+        Sincerely,<br />
+        The <span className="font-semibold">Applio</span> Team.
+      </p>
+    </div>
       </div>
       </div>
     </main>

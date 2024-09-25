fdecd1a7e58a681cc517d9e06f73c1019791026e
Blaise <blaise@applio.org>
2024-09-25 23:44:07 +0200

    style(org): minor changes on playground

--- apps/applio-org/src/app/products/playground/page.tsx
+++ apps/applio-org/src/app/products/playground/page.tsx
@@ -1,13 +1,18 @@
 export default function Playground() {
 	return (
 		<main className="w-full h-[96svh] mt-12 flex p-8 flex-col gap-2"> 
-			<a className="px-6 py-1 w-fit flex justify-end ml-auto bg-white text-black rounded-md border border-white/20 slow hover:bg-white/80" href="/products/rvc">Playground</a>
 			<div className="w-full h-full">
 			<iframe
 				title="Applio Playground"
 				src="https://iahispano-applio.hf.space"
-				className="w-full h-full border rounded-xl border-white/20 p-1 shadow-2xl shadow-white/10"
+				className="w-full h-full rounded-xl"
 			/>
+
+				<a href="https://huggingface.co/spaces/IAHispano/Applio" target="_blank" rel="noreferrer">
+					<p className="text-center text-sm text-gray-500">
+						This is a sandbox for experimenting with Applio. To avoid long wait times, please duplicate the playground and use your own copy.
+					</p>
+				</a>
 			</div>
 		</main>
 	);

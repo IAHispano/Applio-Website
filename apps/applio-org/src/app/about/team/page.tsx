dccefd98faabb9359e4b2ede9a927210c2bf0ab3
Blaise <blaise@applio.org>
2024-10-22 21:15:04 +0200

    feat(org): fix mobile and improve our team page

--- apps/applio-org/src/app/about/team/page.tsx
+++ apps/applio-org/src/app/about/team/page.tsx
@@ -3,14 +3,11 @@ import Team from "@/components/team/team-members";
 export default function About() {
 	return (
 		<>
-			<div className="flex flex-col justify-center items-start mx-auto w-full max-w-4xl">
-				<h1 className="text-4xl font-semibold mt-24 text-left">Team</h1>
-				<p className="text-sm text-neutral-200 text-left pl-1.5">
-					Meet the people why make Applio happen.
-				</p>
+			<div className="bg-gradient-to-b from-[#FFFFFF]/20 to-transparent max-md:mx-full w-full h-[20svh] md:-mt-[5svh] md:px-[25svh] py-[15svh] flex flex-col gap-24 max-md:px-12">
+				<h1 className="text-7xl text-center font-bold">Our Team</h1>
 			</div>
 			<main className="min-h-screen flex flex-col items-center justify-center">
-				<div className="flex flex-col justify-center mx-auto items-center py-12 max-w-5xl">
+				<div className="flex flex-col justify-center mx-auto items-center py-9 max-md:px-12 max-w-5xl">
 					<Team />
 				</div>
 			</main>

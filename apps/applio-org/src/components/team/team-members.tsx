5624f14d3fd623cf101fedaa9f399043e875928a
Blaise <blaise@applio.org>
2024-10-22 21:15:39 +0200

    feat(org): minor changes to team component

--- apps/applio-org/src/components/team/team-members.tsx
+++ apps/applio-org/src/components/team/team-members.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
 import { supabase } from "@/utils/database";
+import Spinner from "@/components/layout/spinner";
 
 export default function Team() {
 	const [error, setError] = useState<string>();
@@ -42,29 +42,29 @@ export default function Team() {
 	if (loading || !data || data.length === 0) {
 		return (
 			<div>
-				<div className="text-white h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-success motion-reduce:animate-[spin_1.5s_linear_infinite]"></div>
+				<Spinner/>
 			</div>
 		);
 	}
 
 	return (
 		<div className="flex justify-center items-center flex-col w-full">
-			<div className="grid grid-cols-3 gap-12 w-full">
+			<div className="grid grid-cols-3 gap-6 w-full">
 				{data.map((item: any) => (
 					<a
 						className="flex flex-col justify-center items-center"
 						key={item.id}
-						href={`/${item.full_name}`}
+						href={`/@${item.full_name}`}
 					>
 						<img
-							className="m-0 inline-flex rounded-md object-cover !xs:w-36 !xs:h-36 !sm:w-40 !sm:h-40 h-32 w-32 cursor-pointer slow hover:opacity-80"
+							className="m-0 inline-flex rounded-md object-cover w-32 h-32 xs:w-36 xs:h-36 sm:w-40 sm:h-40 cursor-pointer slow hover:opacity-80"
 							src={item.avatar_url || "/favicon.ico"}
 							alt={item.full_name}
 							onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
 								e.currentTarget.src = "/favicon.ico";
 							}}
 						/>
-						<h3 className="mb-0 mt-4 text-white">{item.full_name}</h3>
+						<h3 className="mb-0 mt-3 text-white">{item.full_name}</h3>
 					</a>
 				))}
 			</div>

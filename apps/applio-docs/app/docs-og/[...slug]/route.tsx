4ba2e8f09829310d43dcb5eb52ba0539e1942297
bygimenez <davidalbal123@gmail.com>
2024-11-06 19:20:59 +0100

    feat(docs): :sparkles: add og custom component

--- apps/applio-docs/app/docs-og/[...slug]/route.tsx
+++ apps/applio-docs/app/docs-og/[...slug]/route.tsx
@@ -1,14 +1,12 @@
 import { metadataImage } from '@/utils/metadata-image';
-import { generateOGImage } from 'fumadocs-ui/og';
 import { type ImageResponse } from 'next/og';
+import { generateOGImage } from './og';
 
 export const GET = metadataImage.createAPI((page): ImageResponse => {
   return generateOGImage({
     title: page.data.title,
     description: page.data.description || 'Applio Documentation',
-    site: 'Applio Documentation',
-    primaryColor: '#00AA68',
-    primaryTextColor: '#ffffff',
+    site: 'Applio Documentation'
   });
 });
 

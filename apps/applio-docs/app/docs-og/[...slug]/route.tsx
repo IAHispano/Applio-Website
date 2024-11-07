80f541c8e485c56d071f7aeee886c41022863e20
bygimenez <davidalbal123@gmail.com>
2024-11-07 19:46:54 +0100

    fix(docs): :ambulance: fix build?

--- apps/applio-docs/app/docs-og/[...slug]/route.tsx
+++ apps/applio-docs/app/docs-og/[...slug]/route.tsx
@@ -1,13 +1,14 @@
 import { metadataImage } from '@/utils/metadata-image';
+import { generateOGImage } from 'fumadocs-ui/og';
 import type { ImageResponse } from 'next/og';
-import { generateOGImage } from './og';
-
 export const runtime = 'edge';
 
 export const GET = metadataImage.createAPI((page): ImageResponse => {
   return generateOGImage({
     title: page.data.title,
     description: page.data.description || 'Applio Documentation',
-    site: 'Applio Documentation'
+    site: 'Applio Documentation',
+    primaryColor: 'rgba(0,170,140,0.5)',
+    primaryTextColor: 'rgb(255, 255, 255)',
   });
 });

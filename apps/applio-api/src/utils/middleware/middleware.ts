0ea26c4b77ea9108e6d1ba533a27d6513a85c4ac
bygimenez <davidalbal123@gmail.com>
2024-11-07 19:18:10 +0100

    feat(api): :sparkles: add api v2

---
+++ apps/applio-api/src/utils/middleware/middleware.ts
@@ -0,0 +1,24 @@
+import type { Context } from 'hono';
+import { initializeSupabase } from '../database';
+
+export const authenticate = async (c: Context, next: () => Promise<void>) => {
+    const supabase = initializeSupabase(c);
+
+	const token = c.req.header('Authorization');
+
+	if (!token) {
+		return c.json({ error: 'Unauthorized: No token provided.' }, 401);
+	}
+
+	const { data, error } = await supabase
+		.from('tokens')
+		.select('*')
+		.eq('token', token)
+		.single();
+
+	if (error || !data) {
+		return c.json({ error: 'Unauthorized: Invalid token' }, 401);
+	}	
+
+	await next();
+};

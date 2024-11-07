1ee72204870e181add56c27e7418bbe9df5dfc2a
bygimenez <davidalbal123@gmail.com>
2024-11-07 19:49:28 +0100

    fix(docs): :ambulance: remove generate og image

--- apps/applio-docs/utils/metadata-image.ts
+++
@@ -1,7 +0,0 @@
-import { source } from '@/lib/source';
-import { createMetadataImage } from 'fumadocs-core/server';
-
-export const metadataImage = createMetadataImage({
-  source,
-  imageRoute: 'docs-og',
-});

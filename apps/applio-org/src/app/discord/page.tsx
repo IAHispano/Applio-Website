cc132793b2345482bc06ddbd896bc81ebd8324ca
bygimenez <davidalbal123@gmail.com>
2024-11-27 15:52:42 +0100

    add discord.applio.org

---
+++ apps/applio-org/src/app/discord/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { redirect } from "next/navigation"
+import { useEffect } from "react"
+
+export default function Discord() {
+    useEffect(() => {
+        redirect("https://discord.gg/urxFjYmYYh")
+    }, [])
+    
+    return (
+        <main className="mt-24 flex justify-center items-center flex-col m-auto">
+            <h1 className="text-xs text-neutral-300">Redirecting... if it doesn't do it automatically <a href="https://discord.gg/urxFjYmYYh" className="text-xs text-neutral-200 underline hover:text-white slow">click here</a>.</h1>
+            
+        </main>
+    )
+}
+
